0b7b628d0a8a70ed33ac76c2ec65461ab310ec0b
Martina Kušnierová <kusnierova@gmail.com>
2017-11-05 07:46:27 +0000

    svg map in react components

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Map from "./map";
+import './index.css';
+
+// ======================================== <img src={"mapall.svg"} alt= "Map"/>
+
+ReactDOM.render(
+  <Map />,
+//  <img src={"mapall.svg"} alt= "Map"/>,
+  document.getElementById('root')
+);
+
+//ReactDOM.render(
+//  <img src={"https://s.cdpn.io/3/kiwi.svg"}/>,
+//  document.getElementById('root')
+//);
